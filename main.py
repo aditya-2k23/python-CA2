d70f5792bc0f76139cf7c52ed69a593245b6f1ff
aditya-2k23 <holaaditya123@gmail.com>
2025-04-07 13:01:10 +0530

    Data cleaning process.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Problem statement
 """
-The video game industry is highly competitive, with numerous titles released across various consoles, genres, and regions. Companies need to understand the factors that contribute to a game's commercial success, critical reception, and regional market performance to optimize their development, marketing, and distribution strategies. Using the provided dataset, analyze the relationships between game attributes (e.g., genre, console, publisher, developer, critic score) and their sales performance (total and regional) to identify trends, patterns, and actionable insights that can guide decision-making in the gaming industry.
+Video Game Sales: Understanding the key factors that influence game sales across regions, platforms, genres, and time by analyzing critic scores, publisher impact, and release patterns to uncover trends that drive commercial success in the gaming industry.
 """
 
 import pandas as pd
@@ -23,3 +23,22 @@ print("Summarizing the data:")
 print(df.describe())
 print("Columns in the dataset:")
 print(df.columns.tolist())
+
+# Handling missing values
+df = df.drop(columns=['last_update'])
+
+# Handling missing values in the 'year_of_release' column
+sales_columns = ['total_sales', 'na_sales', 'jp_sales', 'pal_sales', 'other_sales']
+df[sales_columns] = df[sales_columns].fillna(0)
+
+# Dropping rows with missing values in 'critic_score'
+df_critic = df.dropna(subset=['critic_score'])
+
+# Extracting the year and month from 'relase_date'
+df['release_year'] = pd.to_datetime(df['release_date']).dt.year
+df['release_month'] = pd.to_datetime(df['release_date']).dt.month
+
+# Keep only the relevant columns
+key_columns = ['title', 'console', 'genre', 'publisher', 'developer', 'critic_score', 'release_date', 'release_year', 'release_month'] + sales_columns
+
+clean_df = df[key_columns]
